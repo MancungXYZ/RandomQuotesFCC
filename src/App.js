7e86dca76bc84e05edd61b108e2ebe69ba3a0ae4
Mancung <60146927+MancungXYZ@users.noreply.github.com>
2023-12-14 14:07:10 +0700

    storylinefcc

--- src/App.js
+++ src/App.js
@@ -34,17 +34,19 @@ function App() {
           maxWidth: '100%',
           bgcolor: "blue"
         }}>
-          <Card sx={{ maxWidth: 1000, borderRadius: "10px" }}>
+          <Card id="quote-box" sx={{ maxWidth: 1000, borderRadius: "10px" }}>
             {quotes.map((quote) =>
-              <CardActionArea key={quote._id}>
+              <CardActionArea sx={{
+                padding: "40px"
+              }} key={quote._id}>
                 <CardContent>
                   <Typography gutterBottom variant="h2" component="div" textAlign={"center"}>
                     Quotes of the day
                   </Typography>
-                  <Typography variant="h6" color="text.secondary" textAlign={"center"}>
+                  <Typography id="text" variant="h6" color="text.secondary" textAlign={"center"}>
                     {quote.content}
                   </Typography>
-                  <Typography mt={5} variant="h5" color="text.secondary" textAlign={"right"}>
+                  <Typography id="author" mt={5} variant="h5" color="text.secondary" textAlign={"right"}>
                   {quote.author}
                   </Typography>
                 </CardContent>
@@ -56,17 +58,19 @@ function App() {
                 padding: "10px",
                 display: "flex",
               }}>
-              <IconButton aria-label="delete">
+              <IconButton aria-label="speech">
                 <VolumeUpIcon/>
               </IconButton>
-              <IconButton aria-label="delete">
+              <IconButton aria-label="copy">
                 <ContentCopyIcon/>
               </IconButton>
-              <IconButton aria-label="delete">
-                <TwitterIcon/>
-              </IconButton>
+              <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">
+                <IconButton aria-label="twitter">
+                  <TwitterIcon/>
+                </IconButton>
+              </a>
               
-              <Button onClick={ambilData} sx={{
+              <Button id="new-quote" onClick={ambilData} sx={{
                   display: "flex",
                   justifyContent: "flex-end"
               }} variant="contained">New Quotes</Button>
