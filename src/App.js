b3fe45d0e6588e939c79b9b139cc0e46ea6a59ba
Mancung <60146927+MancungXYZ@users.noreply.github.com>
2023-12-14 13:33:01 +0700

    SimpleReactRandomQuote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Divider, IconButton, Button, Card, CardContent } from "@mui/material";
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    ambilData()
+    console.log(quotes)
+  }, [])
+
+  const ambilData = async () => {
+    try {
+      await axios.get("https://api.quotable.io/quotes/random").then((response) => {
+        setQuotes(response.data);
+      })
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Box sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          maxWidth: '100%',
+          bgcolor: "blue"
+        }}>
+          <Card sx={{ maxWidth: 1000, borderRadius: "10px" }}>
+            {quotes.map((quote) =>
+              <CardActionArea key={quote._id}>
+                <CardContent>
+                  <Typography gutterBottom variant="h2" component="div" textAlign={"center"}>
+                    Quotes of the day
+                  </Typography>
+                  <Typography variant="h6" color="text.secondary" textAlign={"center"}>
+                    {quote.content}
+                  </Typography>
+                  <Typography mt={5} variant="h5" color="text.secondary" textAlign={"right"}>
+                  {quote.author}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              )}
+          <CardContent>
+              <Divider />
+              <Box sx={{
+                padding: "10px",
+                display: "flex",
+              }}>
+              <IconButton aria-label="delete">
+                <VolumeUpIcon/>
+              </IconButton>
+              <IconButton aria-label="delete">
+                <ContentCopyIcon/>
+              </IconButton>
+              <IconButton aria-label="delete">
+                <TwitterIcon/>
+              </IconButton>
+              
+              <Button onClick={ambilData} sx={{
+                  display: "flex",
+                  justifyContent: "flex-end"
+              }} variant="contained">New Quotes</Button>
+              </Box>
+            </CardContent>  
+        </Card>
+        </Box>
   );
 }
 
